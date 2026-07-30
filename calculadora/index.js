cca53ea5833835103d2d53a8f611084119a84cdd
Guilherme-code10 <moraisaugustoguilherme2006@gmail.com>
2026-07-30 09:07:01 -0300

    finaliza este curso

--- calculadora/index.js
+++ calculadora/index.js
@@ -1,17 +1,32 @@
-const readline = require('readline');
+// const readline = require('readline');
+import { createInterface } from 'readline'
+import { soma, subtracao } from './operacoesMatematicas.js'
 
-const leitor = readline.createInterface({
+const leitor = createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
 leitor.question('Digite  o primeiro número: \n', (numero1) => {
-    leitor.question('Digite a operação:\n+: soma\b-:subtração\n> ', (operacao) => {
+    leitor.question('Digite a operação:\n+: soma\n-:subtração\n> ', (operacao) => {
         leitor.question('Digite o segudo número:\n>', (numero2) => {
   
-            const num1 = Number(numero1)
+            const num1 = Number(numero1) 
             const num2 = Number(numero2)
 
+            let resultado = null
+
+            if(operacao == '+') {
+                resultado = soma(num1, num2)
+            } else if (operacao == '-') {
+                resultado = subtracao(num1, num2)
+            } else {
+                console.log('Operação inválida!')
+            }
+
+            if (resultado != null) {
+                console.log('O resultado da operação é:', resultado)
+            }
 
           leitor.close();
         })
--- calculadora/operaoesMatematicas.js
+++
@@ -1,7 +0,0 @@
-function soma(num1, num2) {
-    return  num1 + num2
-}
-
-function subtração(num1, num2) {
-    return num1 - num2
-}
